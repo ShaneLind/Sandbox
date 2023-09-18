68e516b0505f6d938889ccf0d332911ed6b8c232
ShaneLind <shane.lind@my.jcu.edu.au>
2023-09-18 14:47:07 +1000

    Display list of files

---
+++ list_files.py
@@ -0,0 +1,10 @@
+"""
+Shane Lind
+"""
+import os
+
+print(f"The files and folders in {os.getcwd()} are:")
+items = os.listdir('.')
+for item in items:
+    prefix = "(d) " if os.path.isdir(item) else "(f) "
+    print(f"{prefix}\t{item}")
